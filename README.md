698a04b5b8157b877a1e7768f4904343076d8e90
ylqi007 <yunlongqi@my.unt.edu>
2019-11-07 13:04:59 -0600

    11/06/2019, add utils.py to preprocess data

--- README.md
+++ README.md
@@ -4,19 +4,27 @@
 1.1 
 
 - [ ] Crash ID  -->  
-- [ ] Adjusted Average Daily Traffic Amount --> 
-- [ ] Commercial Motor Vehicle Flag
-- [ ] Crash Death Count 
-- [ ] Crash Time
-- [ ] Crash Total Injury Count
-- [ ] Curve Degrees
-- [ ] Curve Type
-- [ ] Day of Week
-- [ ] Inside Shoulder Width on Divided Highway
-- [ ] Latitude
-- [ ] Light Condition
-- [ ] Manner of Collision
-- [ ] Number of Lanes
-- [ ] Object Struck
-- [ ] Outside Shoulder Width on Divided Highway
-- [ ] Property Damages
+- [ ] Adjusted Average Daily Traffic Amount         --> (No Data, 62579)
+- [ ] Commercial Motor Vehicle Flag                 --> (No, Yes) ==> (0, 1)
+- [ ] Crash Death Count                             --> (0,1,2,...) ==> int
+- [ ] Crash Time                                    --> (2329, 1033) --> 1./(24*12) float, consider 5 min as an unit
+- [ ] Crash Total Injury Count                      --> (0, 1, 2, ..) ==> int
+- [ ] Curve Degrees                                 --> (0, 1, 2, 3, 4) ==> ???
+- [ ] Curve Type                                    --> (No Data, N) ==> ???
+- [ ] Day of Week                                   --> (Monday, Tue...) ==> (0, 1, 2,...) ==> (0, 1/7, ...6/7)???
+- [ ] Inside Shoulder Width on Divided Highway      --> (No Data, Null, 0, 10, 11, 16) ==> ???
+- [ ] Latitude                                      --> (32.6628643, No Data) ==> keep it original
+- [ ] Light Condition                               --> (1-Daylight, 3, Dark-lighted, 99-unknown) ==> (0, 1, 2, 3, 99)
+- [ ] Longitude
+- [x] Manner of Collision                           --> ()
+- [ ] Number of Lanes                               --> (No Data, 4, 6, 7, 8,) ==> Integer
+- [x] Object Struck                                 --> ???
+- [ ] Outside Shoulder Width on Divided Highway     --> (No Data, 0, 10, ) ==> int or float?
+- [x] Property Damages                              --> (None, Guard Rail, Utility Pol...) ==> ?
+- [ ] Road Class                                    --> (City Street, Interstate, US & State highways) ==> (1, 2, 3)
+- [x] Street Name
+- [x] Street Number
+- [ ] Surface Condition                             --> (1-Dry, 6-ICE, 99-UNKNOWN)
+- [ ] Weather Condition                             --> (1-clear, 2-cloudy,...)
+- [ ] Diver Alcohol Result                          --> (No Data, 1-POSITIVE, 1)
+
---
+++ src/utils.py
@@ -0,0 +1,260 @@
+import numpy as np
+
+
+def _preprocess_Crash_ID(crash_id):
+    pass
+
+
+def preprocess_Adjusted_Average_Daily_Traffic_Amount(amount):
+    for idx, item in enumerate(amount):
+        if item == 'No Data':
+            amount[idx] = 0
+        else:
+            amount[idx] = int(item)
+    return np.array(amount)
+
+
+def preprocess_Commercial_Motor_Vehicle_Flag(flag):
+    """
+    Convert boolean to int.
+        No --> 0
+        Yes --> 1
+    :param flag:
+    :return:
+    """
+    for idx, item in enumerate(flag):
+        if item == 'No':
+            flag[idx] = 0
+        elif item == 'Yes':
+            flag[idx] = 1
+        else:
+            raise ValueError('Unexpected Value.')
+
+    return np.array(flag)
+
+
+def preprocess_Crash_Death_Count(count):
+    count = [int(c) for c in count]
+    return np.array(count)
+
+
+def preprocess_Crash_Time(time):
+    for idx, item in enumerate(time):
+        item = int(item)
+        item = item // 100 * 60 + item % 100
+        item = item // 10
+        time[idx] = item
+    return np.array(time)
+
+
+def preprocess_Crash_Total_Injury_Count(count):
+    for idx, item in enumerate(count):
+        count[idx] = int(item)
+    return np.array(count)
+
+
+def preprocess_Curve_Degrees(degree):
+    print(set(degree))
+    for idx, item in enumerate(degree):
+        if item == 'No Data':
+            degree[idx] = 0
+        else:
+            degree[idx] = int(item)
+    return np.array(degree)
+
+
+def preprocess_Curve_Type(types):
+    for idx, item in enumerate(types):
+        if item == 'S':
+            types[idx] = 0
+        elif item == 'N':
+            types[idx] = 1
+        else:
+            types[idx] = 0.5
+    return np.array(types)
+
+
+def preprocess_Day_of_Week(day):
+    for idx, item in enumerate(day):
+        if item == 'MONDAY':
+            day[idx] = 1
+        elif item == 'TUESDAY':
+            day[idx] = 2
+        elif item == 'WEDNESDAY':
+            day[idx] = 3
+        elif item == 'THURSDAY':
+            day[idx] = 4
+        elif item == 'FRIDAY':
+            day[idx] = 5
+        elif item == 'SATURDAY':
+            day[idx] = 6
+        elif item == 'SUNDAY':
+            day[idx] = 7
+        else:
+            raise ValueError('Unexpected value.')
+    return np.array(day)
+
+
+def preprocess_Inside_Shoulder_Width_on_Divided_Highway(width):
+    for idx, item in enumerate(width):
+        if item == 'No Data':
+            width[idx] = 0
+        else:
+            width[idx] = int(item)
+    return np.array(width)
+
+
+def preprocess_Latitude(latitude):
+    for idx, item in enumerate(latitude):
+        if item == 'No Data':
+            latitude[idx] = 0
+        else:
+            latitude[idx] = float(item)
+    return np.array(latitude)
+
+
+def preprocess_Light_Condition(light):
+    for idx, item in enumerate(light):
+        if item == '1 - DAYLIGHT':
+            light[idx] = 1
+        elif item == '2 - DARK, NOT LIGHTED':
+            light[idx] = 2
+        elif item == '3 - DARK, LIGHTED':
+            light[idx] = 3
+        elif item == '4 - DARK, UNKNOWN LIGHTING':
+            light[idx] = 4
+        elif item == '5 - DAWN':
+            light[idx] = 5
+        elif item == '6 - DUSK':
+            light[idx] = 6
+        else:
+            light[idx] = 0
+    return np.array(light)
+
+
+def preprocess_Longitude(longitude):
+    for idx, item in enumerate(longitude):
+        if item == 'No Data':
+            longitude[idx] = 0
+        else:
+            longitude[idx] = float(item)
+    return np.array(longitude)
+
+
+def preprocess_Manner_of_Collision(manner):
+    print('=== manner ===')
+    print(set(manner))
+    pass
+
+
+def preprocess_Number_of_Lanes(num_of_lanes):
+    num_of_lanes = [0 if item == 'No Data' else int(item) for item in num_of_lanes]
+    return np.array(num_of_lanes)
+
+
+def preprocess_Object_Struck(struck):
+    print('=== struck ===')
+    print(set(struck))
+    pass
+
+
+def preprocess_Outside_Shoulder_Width_on_Divided_Highway(width):
+    width = [0 if item == 'No Data' else int(item) for item in width]
+    return np.array(width)
+
+
+def preprocess_Property_Damages(property):
+    print('=== property damages ===')
+    print(set(property))
+    pass
+
+
+def preprocess_Road_Class(road_class):
+    for idx, item in enumerate(road_class):
+        if item == 'INTERSTATE':
+            road_class[idx] = 0
+        elif item == 'US & STATE HIGHWAYS':
+            road_class[idx] = 1
+        elif item == 'TOLLWAY':
+            road_class[idx] = 2
+        elif item == 'NON TRAFFICWAY':
+            road_class[idx] = 3
+        elif item == 'OTHER ROADS':
+            road_class[idx] = 4
+        elif item == 'COUNTY ROAD':
+            road_class[idx] = 5
+        elif item == 'CITY STREET':
+            road_class[idx] = 6
+        elif item == 'FARM TO MARKET':
+            road_class[idx] = 7
+    return np.array(road_class)
+
+
+def preprocess_Street_Name(street_name):
+    print('==== street name ===')
+    print(set(street_name))
+    pass
+
+
+def preprocess_Street_Number(street_num):
+    print('==== street num ===')
+    print(set(street_num))
+    pass
+
+
+def preprocess_Surface_Condition(condition):
+    for idx, item in enumerate(condition):
+        if item == '1 - DRY':
+            condition[idx] = 1
+        elif item == '2 - WET':
+            condition[idx] = 2
+        elif item == '3 - STANDING WATER':
+            condition[idx] = 3
+        elif item == '4 - SNOW':
+            condition[idx] = 4
+        elif item == '5 - SLUSH':
+            condition[idx] = 5
+        elif item == '6 - ICE':
+            condition[idx] = 6
+        elif item == '7 - SAND, MUD, DIRT':
+            condition[idx] = 7
+        else:
+            condition[idx] = 0
+    return np.array(condition)
+
+
+def preprocess_Weather_Condition(weather):
+    for idx, item in enumerate(weather):
+        if item == '1 - CLEAR':
+            weather[idx] = 1
+        elif item == '2 - CLOUDY':
+            weather[idx] = 2
+        elif item == '3 - RAIN':
+            weather[idx] = 3
+        elif item == '4 - SLEET/HAIL':
+            weather[idx] = 4
+        elif item == '5 - SNOW':
+            weather[idx] = 5
+        elif item == '6 - FOG':
+            weather[idx] = 6
+        elif item == '7 - BLOWING SAND/SNOW':
+            weather[idx] = 7
+        elif item == '8 - SEVERE CROSSWINDS':
+            weather[idx] = 8
+        else:
+            weather[idx] = 0
+
+
+def preprocess_Driver_Alcohol_Result(alcohol):
+    for idx, item in enumerate(alcohol):
+        if item == '1 - POSITIVE':
+            alcohol[idx] = 1
+        elif item == '2 - NEGATIVE':
+            alcohol[idx] = 0
+        else:
+            alcohol[idx] = 0
+    return np.array(alcohol)
+
+
+def _unique(values):
+    print(set(values))
